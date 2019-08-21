51f49b50e8c2ba0963f43e35b57a0cc66b8a191a
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2019-08-21 13:38:31 -0300

    gitignore adicionado e babelrc

--- sum.test.js
+++ sum.test.js
@@ -1,10 +1,19 @@
 
 'use strict'
 
-const expect = require('chai').expect
-const sum = require('./sum')
+//modo es6 -2015
+import  {expect}  from 'chai'
+import sum from './sum'
+
+
+// const expect = require('chai').expect
+// const sum = require('./sum')
 
 it('sum should be a function',()=>{
   //a const é uma funcao?
   expect(sum).to.be.a('function')
+})
+
+it('Soma de 1 mais 2 tem que',()=>{
+  expect(sum(1,2)).to.be.equal(3 )
 })
